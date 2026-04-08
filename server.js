6018051f14f5efd8850ebab2a846517008b8aaf5
liasafronova2-spec <liasafronova2@gmail.com>
2026-04-08 13:49:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = 'mogilev-secret-key-2026';
